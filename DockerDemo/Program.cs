ad09272f44b173b98226377bdfd8cd9e54b97742
CheneyZhe <564614731@qq.com>
2021-03-01 17:04:42 +0800

    docker

--- DockerDemo/Program.cs
+++ DockerDemo/Program.cs
@@ -6,7 +6,7 @@ namespace DockerDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Docker!");
         }
     }
 }
