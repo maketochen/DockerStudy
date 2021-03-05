63833b835d8f6c0a1b728a42229439699042c6c8
CheneyZhe <564614731@qq.com>
2021-03-06 00:32:53 +0800

    test api

---
+++ FakeTravel.API/FakeTrave.API/Controllers/TestApiController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+ 
+
+namespace FakeTrave.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestApiController : ControllerBase
+    {
+        // GET: api/<Test>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<Test>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<Test>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<Test>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<Test>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
